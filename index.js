3324328c44df73977e5e70bdc4c6540dea1d459c
Im-Abhi <abhishekgupta942001@gmail.com>
2021-11-15 21:42:10 +0530

    Backend starting files

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get("/", (req,res)=>{
+    res.send("Server Working!");
+});
+
+app.listen(4000, ()=>{
+    console.log("Server started at port 4000");
+});

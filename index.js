fd4a5cc3d29af91fffc30874cac74f7a2c2a2a5d
Im-Abhi <abhishekgupta942001@gmail.com>
2021-11-19 17:27:44 +0530

    Create localDB
    Connect with localDB using dummy data!

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
 const express = require('express');
-
+const mongoose  = require('mongoose');
+const { model, Schema } = mongoose;
+const Menu = require("./models/menuModel");
 const app = express();
 
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+mongoose.connect(`mongodb://localhost:27017/RestaurantDB`,{ useNewUrlParser: true });
+
+const newMenu = new Menu({
+    menuName: "Special",
+    dishes: {
+        dishName: "Roasted veggies",
+        description: "Serves 8-10",
+        calories: 400,
+        dishImage: "https://i1.wp.com/www.nourishedtheblog.com/wp-content/uploads/2016/03/Balsamic-Honey-Roasted-Vegetables-1.jpg?resize=1543,2160&ssl=1"
+    }
+});
+
 app.get("/", (req,res)=>{
-    res.send("Server Working!");
+    Menu.find({},(err,result)=>{
+        if(result.length === 0){
+            Menu.insertMany(newMenu, (err)=>{
+                if(err) console.log(err);
+                else    console.log("Inserted Successfully");
+            });
+        }
+    });
+    res.send("Created DB!");
 });
 
 app.listen(4000, ()=>{
